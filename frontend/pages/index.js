f543e4485cdbf566d8e29d18ba8a5fa42135a31d
t-probe <tristan.schumann@outlook.com>
2025-03-09 17:49:50 +0000

    Tried to change some home page things, didn't work

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -12,33 +12,3 @@ export default function Home() {
     </Layout>
   );
 }
-
-import Link from "next/link";
-
-export default function Home() {
-  // Dummy data for active markets (you'll replace this with real contract data later)
-  const markets = [
-    { id: 1, question: "Will the price of ETH go up by the end of the year?" },
-    { id: 2, question: "Will Tesla release a new model this year?" },
-    { id: 3, question: "Will the next Apple product have a foldable screen?" },
-  ];
-
-  return (
-    <div>
-      <h1>Prediction Markets</h1>
-      <h2>Active Markets</h2>
-      <ul>
-        {markets.map((market) => (
-          <li key={market.id}>
-            <Link href={`/market/${market.id}`}>
-              <a>{market.question}</a>
-            </Link>
-          </li>
-        ))}
-      </ul>
-      <Link href="/create-market">
-        <a>Create a New Market</a>
-      </Link>
-    </div>
-  );
-}
